924283f59fd873f3e133ae9600f3f5937813f73f
LukasDL <lukashevich.dmitry.e@gmail.com>
2023-03-10 20:31:01 +0500

    Ball is Reflect

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,23 +6,16 @@ public class Ball : MonoBehaviour
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private Transform _ballPoint;
     [SerializeField] bool _beginGameCheck;
-    [SerializeField] [Range (5f, 50f)] private float _startForceX;
+    [SerializeField][Range(5f, 50f)] private float _startForceX;
     [SerializeField][Range(5f, 50f)] private float _startForceY;
 
+    [SerializeField] private float _speed = 20f;
+
+    Vector3 lastvelocity;
     void Update()
     {
 
-        if (_beginGameCheck == true)
-        {
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                _beginGameCheck = false;
-                transform.position = _ballPoint.position;
-                _rigidbody.velocity = new Vector3(0f, 0f, 0f);
-            }
-
-        }
-
+     
         if (_beginGameCheck == false)
         {
             transform.position = _ballPoint.position;
@@ -31,12 +24,39 @@ public class Ball : MonoBehaviour
             {
                 _beginGameCheck = true;
                 _rigidbody.AddForce(_startForceX, _startForceY, 0f, ForceMode.VelocityChange);
+
+
             }
 
         }
 
 
+        else if (_beginGameCheck == true)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                _beginGameCheck = false;
+                transform.position = _ballPoint.position;
+                _rigidbody.velocity = Vector3.zero;
+            }
+
+        }
+
+
+       lastvelocity = _rigidbody.velocity;
 
 
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        _rigidbody.velocity = Vector3.Reflect(lastvelocity, collision.contacts[0].normal);
+    
+
+    }
+
+
+
+
 }
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+public class Laser : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _laser;
+    [SerializeField] private float _lengh = 25f;
+    [SerializeField] private float _scaleSpeed = 10f;
+    bool _laserOn = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _laserOn = true;
+        }
+
+        if (_laserOn == true)
+        {
+            LaserActivate(_laser, _lengh, _scaleSpeed);
+        }
+
+
+    }
+
+
+    public void LaserActivate(GameObject laser, float lengh, float scaleSpeed)
+    {
+
+        _laser.SetActive(true);
+
+        if (laser.transform.localScale.y <= lengh)
+        {
+            _laser.transform.localScale += new Vector3(0f, scaleSpeed, 0f) * Time.deltaTime;
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
